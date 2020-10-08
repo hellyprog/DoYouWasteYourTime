16e345ef4ca57c8574da511e27a58d07a5cefc0a
volodymyr.romaniv <v.b.romaniv@gmail.com>
2020-10-08 10:00:03 +0300

    Added sitemap generator

---
+++ do-you-waste-your-time/src/router.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Route } from 'react-router';
+ 
+export default (
+    <Route>
+        <Route path='/' />
+        <Route path='/contact-us' />
+        <Route path='/test-result' />
+        <Route path='/test' />
+    </Route>
+);
